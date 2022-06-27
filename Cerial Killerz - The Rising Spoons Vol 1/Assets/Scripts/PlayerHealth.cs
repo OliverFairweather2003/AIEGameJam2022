18e784abd24ea6100bd13709d875012816b68b91
The SoulDragon <100251058+TheSoulDragon@users.noreply.github.com>
2022-06-27 13:38:54 +1000

    health script done - jakob

--- Cerial Killerz - The Rising Spoons Vol 1/Assets/Scripts/PlayerHealth.cs
+++ Cerial Killerz - The Rising Spoons Vol 1/Assets/Scripts/PlayerHealth.cs
@@ -4,26 +4,42 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public HealthSlider healthslider;
+
     public int maxHealth = 100;
     public float currentHealth;
 
     private void Start()
     {
         currentHealth = maxHealth;
+        healthslider.SetMaxHealth(currentHealth);
     }
 
     private void Update()
     {
-        currentHealth -= Time.deltaTime / 4;
+        currentHealth -= Time.deltaTime;
+        healthslider.SetHealth(currentHealth);
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        healthslider.SetHealth(currentHealth);
     }
 
     public void AddHealth(int health)
     {
         currentHealth += health;
+        healthslider.SetHealth(currentHealth);
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }
