55a883df9db99ac0b6a1957107c75b8fb53446ce
OliverFairweather2003 <90094738+OliverFairweather2003@users.noreply.github.com>
2022-06-28 14:49:13 +1000

    Double Jump!

--- Cerial Killerz - The Rising Spoons Vol 1/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Cerial Killerz - The Rising Spoons Vol 1/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    int jumpCount = 0;
+    public int maxJumps = 2;
     public float MovementSpeed = 2.0f;
     public float JumpSpeed = 4f;
     public float groundRadiusCheck = 0.3f;
     public LayerMask layers;
     public float gravityMultiplier = 4f;
 
+    public SpoonThrow spoonThrow;
+
+    public GameObject spoonPrefab;
+
     public Transform model;
+
+
 
     CharacterController charControl;
     float moveInput;
@@ -27,11 +35,25 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (spoonThrow == null)
+                spoonThrow = Instantiate(spoonPrefab, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), transform.rotation).GetComponent<SpoonThrow>();
+
+            spoonThrow.transform.position =  new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            spoonThrow.transform.rotation = transform.rotation;
+            spoonThrow.gameObject.SetActive(true);
+            spoonThrow.Throw();
+           // GameObject clone;
+            //
+        }
+
         moveInput = Input.GetAxis("Horizontal");
-        jumpInput = Input.GetButton("Jump");
+        jumpInput = Input.GetButtonDown("Jump");
 
         Move();
     }
+
     void Move()
     {
         currentVelocity.x = moveInput * MovementSpeed ;
@@ -46,7 +68,10 @@ public class PlayerMovement : MonoBehaviour
         }
 
         if(charControl.isGrounded)
+         {
             currentVelocity.y = 0;
+            jumpCount = 0;
+        }
 
         float tempGravMultiplier =1;
 
@@ -57,8 +82,9 @@ public class PlayerMovement : MonoBehaviour
 
 
 
-        if(jumpInput == true && charControl.isGrounded == true)
+        if(jumpInput == true && jumpCount < maxJumps)
         {
+            jumpCount++;
             currentVelocity.y = JumpSpeed;
         }
 
@@ -69,5 +95,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 pos = transform.position;
         pos.z =0;
         transform.position = pos;
+
+        //if(gameHasBugs)
+        //{
+        //  FixAllBugs();
+        //}
+        
     }
 }
