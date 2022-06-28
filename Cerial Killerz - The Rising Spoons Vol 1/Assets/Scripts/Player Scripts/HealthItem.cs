d1e5dd908443159932764156b8740e9e6225b7f5
TheBossDG <99941561+TheBossDG@users.noreply.github.com>
2022-06-28 13:33:41 +1000

    Health Item Update

--- Cerial Killerz - The Rising Spoons Vol 1/Assets/Scripts/Player Scripts/HealthItem.cs
+++ Cerial Killerz - The Rising Spoons Vol 1/Assets/Scripts/Player Scripts/HealthItem.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    Rigidbody ridgidBody;
+    public Vector3 hitBoxSize;
+    public Vector3 hitBoxPosition;
+    public LayerMask layers;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ridgidBody = GetComponent < Rigidbody > ();
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position + (Vector3)hitBoxPosition, new Vector3(hitBoxSize.x, hitBoxSize.y, hitBoxSize.z));
     }
 
     public void CheckHitbox()
     {
-        if (colItem.tag == "Player");
-        {
+        Collider[] col = Physics.OverlapBox(transform.position + (Vector3)hitBoxPosition, new Vector3(hitBoxSize.x, hitBoxSize.y, 0), Quaternion.identity, layers);
 
+        foreach (Collider colItem in col)
+        {
+            if (colItem.tag == "Player")
+            {
+                CheckHealth(colItem);
+            }
+        } 
+    }
+
+    public void CheckHealth(Collider col)
+    {
+        PlayerHealth PlayerHealth = col.GetComponent<PlayerHealth>();
+        if (PlayerHealth != null)
+        {
+            PlayerHealth.AddHealth(20);
         }
     }
 
